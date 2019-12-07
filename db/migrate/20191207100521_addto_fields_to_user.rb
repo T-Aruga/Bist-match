fff52b18ac5e0778eeed2a6012c1cc879d031d5e
Aruga <arugatatsuya@aruga.local>
2019-12-07 20:31:00 +0900

    Modify form style

---
+++ db/migrate/20191207100521_addto_fields_to_user.rb
@@ -0,0 +1,11 @@
+class AddtoFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :phone_number, :string
+    add_column :users, :description, :text
+    add_column :users, :image_id, :string
+    add_column :users, :sex, :integer, default: 0, null: false
+    add_column :users, :age, :integer
+    add_column :users, :favorite_store, :string
+    add_column :users, :active, :boolean, default: false, null: false
+  end
+end
