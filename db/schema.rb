2961fda5997b26799054b1a8db2e22398db90485
T-Aruga <baideneo@gmail.com>
2019-12-17 12:43:08 +0900

    Create favorite Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_034407) do
+ActiveRecord::Schema.define(version: 2019_12_17_032847) do
 
   create_table "areas", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "plan_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
