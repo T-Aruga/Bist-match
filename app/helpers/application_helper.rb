67328b80c7d93add3050791d6c1c45c2d35560ed
T-Aruga <baideneo@gmail.com>
2020-01-08 13:09:51 +0900

    Set up rspec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       @notifications[0].user.unread
     end
   end
+
+  def is_ready_plan?
+    !plan.active && !plan.price.blank? && !plan.member.blank? && !plan.jenre_id.blank? && !plan.status.blank? && !plan.area_id.blank? &&
+    !plan.restaurant.name.blank? && !plan.restaurant.address.blank? && !plan.restaurant.line.blank? && !plan.restaurant.station.blank? &&
+    !plan.restaurant.walk.blank? && !plan.summary.blank? && !plan.title.blank? && !plan.requirement.blank? && !plan.photos.blank? &&
+    !plan.plan_date.blank? && !plan.start_time.blank? && !plan.end_time.blank? && !plan.deadline.blank?
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  describe '#age' do
-    context '20年前の生年月日の場合' do
-      let(:user) {User.new(age: Time.zone.now - 20.years)}
+RSpec.describe User, "モデルに関するテスト", type: :model do
+  describe 'アソシエーション' do
+    it "Bookモデルを多数持っている" do
+      is_expected.to have_many(:books)
+    end
+  end
 
-      it '年齢が20才であること' do
-        expect(user.age).to eq 20
+  describe '実際に保存してみる' do
+    context "保存できる場合" do
+      it "画像データなし" do
+        expect(FactoryBot.create(:user)).to be_valid
+      end
+
+      it "画像データあり" do
+        expect(FactoryBot.create(:user, :create_with_image)).to be_valid
+      end
+    end
+    context "保存できない場合" do
+      it "nameが空欄" do
+        expect(FactoryBot.build(:user, :no_name)).to_not be_valid
+      end
+
+      it "nameが１文以下" do
+        expect(FactoryBot.build(:user, :too_short_name)).to_not be_valid
+      end
+
+      it "nameが21文字以上" do
+        expect(FactoryBot.build(:user, :too_long_name)).to_not be_valid
+      end
+
+      it "introductionが51文字以上" do
+        expect(FactoryBot.build(:user, :too_long_introduction)).to_not be_valid
       end
     end
   end
