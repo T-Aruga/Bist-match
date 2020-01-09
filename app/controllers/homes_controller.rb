64958124caf3c6637f9635d210828d956cedbb10
T-Aruga <baideneo@gmail.com>
2020-01-09 15:12:55 +0900

    Modify Search logic

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,16 +4,16 @@ class HomesController < ApplicationController
     @plans = Plan.includes(:restaurant).available.page(params[:page])
   end
 
-
   def search
     # ユーザーが検索した場所を保持する
     if params[:search].present? && params[:search].strip != ""
       session[:location] = params[:search]
     end
 
-    # 近隣のプラン情報を取得する
+    # 検索した場所の近隣のプラン情報を取得する
+    # 情報が取得できなければ、利用可能なプランを全件取得する
     if session[:location] && session[:location] != ""
-      @plans_address = Plan.includes(:restaurant).available.near(session[:location], 3, order: 'distance')
+      @plans_address = Plan.includes(:restaurant).available.near(session[:location], 2, order: 'distance')
       if @plans_address.empty?
         @plans_address = Plan.includes(:restaurant).available.all
       end
@@ -26,21 +26,23 @@ class HomesController < ApplicationController
     @arrPlans = @plans.to_a
 
     # 検索条件に当てはまらないプランを検索結果から除外する
-    if params[:plan_date] && !params[:plan_date].empty?
+    # プラン開催日による検索
 
+    if params[:plan_date] && params[:plan_date] != ""
       plan_date = Date.parse(params[:plan_date])
+      @arrPlans = @arrPlans.map{|plan| plan if plan.plan_date == plan_date }.compact
+    end
 
-      @arrPlans.each do |plan|
+    # 時間帯（ランチなど）による検索
 
-        if plan.plan_date < Date.today || plan_date != plan.plan_date
-          @arrPlans.delete(plan)
-        end
-      end
+    if params[:period_time] && params[:period_time] != ""
+      period_time = params[:period_time]
+      @arrPlans = @arrPlans.map{|plan| plan if plan.period_time == period_time }.compact
     end
   end
 
   def search_jenre
-
+    # リンクで渡されたパラメータにより、該当するプラン情報を取得する
     if params[:jenre_id].present?
       session[:jenre_id] = params[:jenre_id].to_i
       @plans_address = Plan.joins(:jenre).available.where("jenres.id = ?", session[:jenre_id])
@@ -53,10 +55,19 @@ class HomesController < ApplicationController
       @plans_address = Plan.includes(:restaurant).available.all
     end
 
+    # 検索オブジェクトの生成
     @search = @plans_address.includes(:jenre, :restaurant).ransack(params[:q])
     @plans = @search.result
 
     @arrPlans = @plans.to_a
+
+    # 検索条件に当てはまらないプランを検索結果から除外する
+    # プラン開催日による検索
+
+    if params[:plan_date] && params[:plan_date] != ""
+      plan_date = Date.parse(params[:plan_date])
+      @arrPlans = @arrPlans.map{|plan| plan if plan.plan_date == plan_date }.compact
+    end
 
   end
 end
