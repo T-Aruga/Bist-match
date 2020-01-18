1845d68b1071f50988d1c82d9227f3e6398ceddc
T-Aruga <baideneo@gmail.com>
2020-01-18 12:12:56 +0900

    Add setting file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../config/environment', __dir__)
-
-
+require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
-require 'shoulda-matchers'
+require 'shoulda/matchers'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -24,8 +22,6 @@ require 'shoulda-matchers'
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -35,8 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-
 RSpec.configure do |config|
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -64,8 +60,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
   config.include FactoryBot::Syntax::Methods
+
+  config.include AuthenticationHelper, type: :system
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,8 +1,122 @@
 require 'rails_helper'
+# config.active_support.deprecation = :silenceをconfigのtest.rbに付け加える必要がある
+RSpec.feature "Homeページ、サインアップ、ログイン、ログアウトに関するテスト", type: :system do
+  before do
+    @user = FactoryBot.create(:user, :active_true)
+  end
 
-RSpec.describe 'Home', type: :system do
-  it 'shows greeting' do
-    visit root_path
-    expect(page).to have_content 'Hello World!'
+  feature "サインアップの確認" do
+    before do
+      visit new_user_registration_path
+      find_field('user[fullname]').set("sample taro")
+      find_field('user[email]').set("aa@aa")
+      find_field('user[password]').set("pppppp")
+    end
+
+    scenario "正しくサインアップできているか" do
+      expect {
+        find("input[fullname='commit']").click
+      }.to change(User, :count).by(1)
+    end
+
+    scenario "リダイレクト先は正しいか" do
+      find("input[name='commit']").click
+      expect(page).to have_current_path profile_user_path(User.last)
+    end
+
+    scenario "サクセスメッセージは正しく表示されるか" do
+      find("input[name='commit']").click
+      expect(page).to have_content "アカウント登録が完了しました"
+    end
+  end
+
+  feature "有効でない内容でのサインアップの確認" do
+    before do
+      visit new_user_registration_path
+      find_field('user[fullname]').set(nil)
+      find_field('user[email]').set("bb@bb")
+      find_field('user[password]').set("pppppp")
+      find("input[name='commit']").click
+    end
+    scenario "エラーメッセージは正しく表示されるか" do
+      expect(page).to have_content "入力されていません"
+    end
+  end
+
+  feature "ログインの確認" do
+    before do
+      visit new_user_session_path
+      find_field('user[email]').set(@user.email)
+      find_field('user[password]').set(@user.password)
+      find("input[name='commit']").click
+    end
+
+    scenario "正しくログインして、リダイレクトされているか" do
+      expect(page).to have_current_path root_path
+    end
+
+    scenario "サクセスメッセージは正しく表示されるか" do
+      expect(page).to have_content "ログインしました"
+    end
+  end
+
+  feature "有効でない内容でのログインの確認" do
+    before do
+      visit new_user_session_path
+      find_field('user[email]').set(nil)
+      find_field('user[password]').set(nil)
+      find("input[name='commit']").click
+    end
+    scenario "リダイレクト先は正しいか" do
+      expect(page).to have_current_path new_user_session_path
+    end
+  end
+
+  feature "ログアウトの確認" do
+    before do
+      login(@user)
+      visit root_path
+      all("a[data-method='delete'][href='/users/sign_out']")[0].click
+     # click_on "logout"
+    end
+    scenario "正しくログアウトして、リダイレクトされているか" do
+      expect(page).to have_current_path "/"
+    end
+    scenario "サクセスメッセージは正しく表示されるか" do
+      expect(page).to have_content "ログアウトしました"
+    end
+  end
+
+  feature "ヘッダーのリンクの確認" do
+    scenario "ログイン時" do
+      login(@user)
+      visit root_path
+      expect(page).to have_link "",href: dashboard_path
+      expect(page).to have_link "",href: new_plan_path
+      expect(page).to have_link "",href: plans_path
+      expect(page).to have_link "",href: your_reservation_path
+      expect(page).to have_link "",href: your_entry_path
+      expect(page).to have_link "",href: user_path(@user)
+      expect(page).to have_link "",href: profile_user_path(@user)
+      expect(page).to have_link "",href: conversations_path
+      expect(page).to have_link "",href: revenues_path
+      expect(page).to have_link "",href: payment_method_path
+      expect(page).to have_link "",href: exit_user_path(@user)
+      expect(page).to have_link "",href: destroy_user_session_path
+    end
+
+    scenario "ログアウト時" do
+      visit root_path
+      expect(page).to have_link "",href: new_user_session_path
+      expect(page).to have_link "",href: new_user_registration_path
+      expect(page).to have_link "",href: about_path
+    end
   end
 end
+
+# RSpec.feature 'Home', type: :system do
+#   scenario 'shows greeting' do
+#     visit root_path
+#     expect(page).to have_content 'Hello World!'
+#   end
+# end
